f0a8fc4add59360d3e12db4576f5e43a2ca33cd5
shahed73iu <shahedimamuddin@gmail.com>
2022-05-14 11:45:09 +0600

    Initial  Commit, Project Created

---
+++ MultiTenant/MultiTenant/Data/TenantInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace MultiTenant.Data
+{
+    public class TenantInfo
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string ContactNo { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
